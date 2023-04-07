9157a5696a010e285d9e570e6e2030ada39cb69b
gokcensolmaz <gokcensolmaz99@gmail.com>
2023-04-07 21:58:56 +0300

    Completed triangle functions

--- shape.kt
+++ shape.kt
@@ -1,5 +1,4 @@
 import java.util.Scanner
-
 fun main() {
     var menu = true
     while (menu) {
@@ -22,13 +21,15 @@ fun main() {
             1 -> square(noOfStar)
             2 -> leftTriangle(noOfStar)
             3 -> rightTriangle(noOfStar)
+            4 -> upsideTriangle(noOfStar)
+            5 -> midpointTriangle(noOfStar)
+            6 -> ascDesTriangle(noOfStar)
             7 -> menu = false
             else -> println("Please enter a number in menu. \n")
         }
 
     }
 }
-
 fun square(noOfStar: Int) {
     for (i in 1..noOfStar) {
         for (j in 1..noOfStar) {
@@ -45,7 +46,6 @@ fun leftTriangle(noOfStar: Int){
         println()
     }
 }
-
 fun rightTriangle(noOfStar: Int){
     for (i in 1..noOfStar){
         for (j in noOfStar downTo i){
@@ -55,5 +55,45 @@ fun rightTriangle(noOfStar: Int){
             print("* ")
         }
         println()
+    }
+}
+fun upsideTriangle(noOfStar: Int){
+    for (i in 1..noOfStar){
+        for (j in noOfStar downTo i){
+            print("* ")
+        }
+        println()
+    }
+}
+fun midpointTriangle(noOfStar: Int){
+    for (i in 1..noOfStar){
+        var blank = noOfStar - i
+        for (j in 1..blank){
+            print("  ")
+        }
+        var star = i * 2 - 1
+        for(j in 1..star){
+            print("* ")
+        }
+        println()
+    }
+}
+fun ascDesTriangle(noOfStar: Int){
+    for (i in 1..noOfStar){
+        var mid = noOfStar / 2 + 1
+        if( i < mid){
+            for (j in 1 until 2*i){
+                print("* ")
+            }
+        }else if (i > mid) {
+            for (j in 1 .. 2 * (noOfStar - i) + 1)
+                print("* ")
+        }else{
+            for (j in 1..noOfStar){
+                print("* ")
+            }
+        }
+        println()
+
     }
 }
